c9926216ac4dcdd332874a415c64e2344a5dec51
Sean Rhoades <sean@seanrhoades.com>
2016-03-29 17:54:44 -0400

    removed image from fb omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 
   provider :facebook, "1527704867524598", "96d94e7ee8b9ccc1be661413ed9cf9de",
-            scope: 'email', info_fields: 'email,name,link,image'
+            scope: 'email', info_fields: 'email,name,link'
 
 
 end
