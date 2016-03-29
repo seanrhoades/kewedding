72fce4ff2b372991212d285bccbd14a02269d19f
Sean Rhoades <sean@seanrhoades.com>
2016-03-29 17:39:06 -0400

    removed twitter login for now

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,7 +3,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, ENV['I4yivOQ10LCzPGztbgJchEMyd'], ENV['V7MKA6tXkGnf4GSwH5sZuk5mS8ytVsS1inFXoO4RmuwT2FFWZ1'], image_size: 'normal'
 
   provider :facebook, "1527704867524598", "96d94e7ee8b9ccc1be661413ed9cf9de",
-            scope: 'email', info_fields: 'email,name,link'
+            scope: 'public_profile', display: 'page', image_size: 'square'
 
 
 end
